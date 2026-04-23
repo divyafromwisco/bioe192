82a6ebb3b4c10975e25d62df91737f89ef6599f9
Divya Chintamani <divya_from_wisco@berkeley.edu>
2026-04-22 19:02:13 -0500

    starting because version control is necessary

---
+++ linear_move_func/linear_move_func.ino
@@ -0,0 +1,37 @@
+const int stepPin = 7;
+const int dirPin  = 4;
+
+const int stepsPerRev = 200;
+const float lead_mm_per_rev = 8.0;   // 8 mm linear travel per revolution
+const float stepsPerMm = stepsPerRev / lead_mm_per_rev;   // 25 steps/mm
+
+void setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+
+// direction: true = up, false = down
+// distance_mm: how far to move linearly
+// stepDelayUs: controls speed
+void moveLinear(bool direction, float distance_mm, int stepDelayUs) {
+  long stepsToMove = round(distance_mm * stepsPerMm);
+
+  digitalWrite(dirPin, direction ? HIGH : LOW);
+
+  for (long i = 0; i < stepsToMove; i++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(stepDelayUs);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(stepDelayUs);
+  }
+}
+
+void loop() {
+  moveLinear(true, 20.0, 500);   // move up 10 mm
+  delay(1000);
+
+  moveLinear(false, 20.0, 500);  // move down 10 mm
+  delay(1000);
+
+  while(true);
+}
